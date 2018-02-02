0a835372063344636c4d3e6e54bef9fa57b42e30
Wim Holvoet <holvoetwim@hotmail.com>
2018-02-02 14:59:59 +0100

    after api

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,6 +4,6 @@ import * as express from "express";
 console.log("hello");
 
 const app  =  express();
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/api/', (req, res) => res.send('Hello !'));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
