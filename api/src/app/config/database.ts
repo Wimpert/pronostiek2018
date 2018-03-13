a8d9f27981f18959589902d4563631289c321cf3
whv2li8 <wim.holvoet@colruytgroup.com>
2018-03-13 10:11:55 +0100

    db connection issue

--- api/src/app/config/database.ts
+++ api/src/app/config/database.ts
@@ -1,12 +1,44 @@
 // config/database.ts
+
+function getDBConf() {
+    console.log("env:");
+    console.log(process.env.NODE_ENV);
+    if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "development"){
+        return {
+            'connection': {
+                'host': 'localhost',
+                'user': 'de_jackies',
+                'password' : 'spelvreugde666'
+         },
+            'database': 'pronostiek',
+            'users_table': 'users'
+        };
+    }
+    return {
+        'connection': {
+            'host': 'localhost',
+            'user' : 'root',
+
+        },
+        'database': 'pronostiek',
+        'users_table': 'users'
+        };
+}
+
+module.exports =  getDBConf();
+
+
+
+
+/*
 module.exports = {
     'connection': {
         'host': 'localhost',
         'user' : 'root'
-        /*'user': 'de_jackies',
-        'password' : 'spelvreugde666'*/
+        /!*'user': 'de_jackies',
+        'password' : 'spelvreugde666'*!/
 
     },
     'database': 'pronostiek',
     'users_table': 'users'
-};
+};*/
